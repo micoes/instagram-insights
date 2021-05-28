e03c55d731d4f04eb9fe1371bc641906ee8b7dee
micoes <79909633+micoes@users.noreply.github.com>
2021-05-28 21:35:56 +0200

    Modul für Instagram Graph API erstellt

--- application/main.py
+++ application/main.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask import render_template
 from flask import request
+
+import scrape
 import json
 
 app = Flask("application")
@@ -12,14 +14,16 @@ def signin():
         user_id = request.form["identifier"]
         user_access_token = request.form["token"]
 
+        # an dieser Stelle einfügen, dass authentication.json nur erstellt wird, wenn "Remember me" angewählt wurde → if, else
         with open("data/authentication.json", "w", encoding="utf-8") as json_file:
             user_data = json.dumps({"user_id": user_id, "user_access_token": user_access_token}, indent=4)
             json_file.write(user_data)
 
-        # an dieser Stelle Code erstellen, welcher scrape.py ausführt, um Daten der jeweils letzten 30 Tage mit der Datenbank abzugleichen
-        # try / except verwenden, für den Fall, dass User ID oder Access Token falsch eingegeben werden (try → insights.html, except → index.html)
-
-        return render_template("insights.html")
+        try:
+            scrape.initial(user_id, user_access_token)
+            return render_template("insights.html")
+        except:
+            return render_template("index.html")
 
     else:
         return render_template("index.html")
--- application/scrape.py
+++ application/scrape.py
@@ -1,42 +1,44 @@
 import requests
 import json
 import datetime
+import os.path
 
-with open("data/authentication.json") as json_file:
-    access = json.load(json_file)
 
-user_id = access["user_id"]
-user_access_token = access["user_access_token"]
+def initial(user_id, user_access_token):
+    # timestamps in API responses use UTC with zero offset and are formatted using ISO-8601
+    # UTC (Universal Time) is 7 hours ahead of PST (Pacific Standard Time)
+    until = datetime.datetime.today()
+    until_processed = until.strftime("%d/%m/%Y, 00:00:00")
+    until_date = datetime.datetime.strptime(until_processed, "%d/%m/%Y, %H:%M:%S")
+    until_timestamp = datetime.datetime.timestamp(until_date)
+    since_timestamp = until_timestamp - 2592000
 
-since, until = "01/01/2021, 00:00:00", "01/10/2021, 23:59:59"
-date_since, date_until = datetime.datetime.strptime(since, "%m/%d/%Y, %H:%M:%S"), datetime.datetime.strptime(until, "%m/%d/%Y, %H:%M:%S")
-timestamp_since, timestamp_until = datetime.datetime.timestamp(date_since), datetime.datetime.timestamp(date_until)
+    # current data about your Instagram Business or Creator Account
+    # https://developers.facebook.com/docs/instagram-api/reference/ig-user
+    instagram_user = requests.get(
+        f"https://graph.facebook.com/v10.0/{user_id}"
+        f"?fields=username,biography,followers_count"
+        f"&access_token={user_access_token}")
 
-# get current data about your Instagram Business or Creator Account
-# https://developers.facebook.com/docs/instagram-api/reference/ig-user
-instagram_user = f"https://graph.facebook.com/v10.0/{user_id}" \
-                 "?fields=id,username,biography,followers_count,follows_count" \
-                 f"&access_token={user_access_token}"
+    # social interaction metrics within range (max. 30 days = 2592000s)
+    # https://developers.facebook.com/docs/instagram-api/guides/insights
+    instagram_reach = requests.get(
+        f"https://graph.facebook.com/v10.0/{user_id}"
+        f"/insights?metric=impressions,reach&period=days_28&since={since_timestamp}&until={until_timestamp}"
+        f"&access_token={user_access_token}")
 
-r = requests.get(instagram_user)
-# print(r.text)
+    # total number of new followers each day of the last 30 days, excluding the current day
+    # https://developers.facebook.com/docs/instagram-api/guides/insights
+    instagram_follower = requests.get(
+        f"https://graph.facebook.com/v10.0/{user_id}"
+        f"/insights?metric=follower_count&period=day&since={since_timestamp}&until={until_timestamp}"
+        f"&access_token={user_access_token}")
 
-# get social interaction metrics about your Instagram Business or Creator Account within range (max. 30 days = 2592000s)
-# https://developers.facebook.com/docs/instagram-api/guides/insights
-insights_impressions_reach = f"https://graph.facebook.com/v10.0/{user_id}" \
-                             f"/insights?metric=impressions,reach&period=days_28&since={timestamp_since}&until={timestamp_until}" \
-                             f"&access_token={user_access_token}"
-
-r = requests.get(insights_impressions_reach)
-# print(r.text)
-
-# get total number of new followers each day for your own Instagram Business or Creator Account within the specified range (max. 1 day = 86400s)
-# supports querying data only for the last 30 days excluding the current day
-timestamp = 1619215200
-
-insights_follower = f"https://graph.facebook.com/v10.0/{user_id}" \
-                    f"/insights?metric=follower_count&period=day&since={timestamp}&until={timestamp + 86400}" \
-                    f"&access_token={user_access_token}"
-
-r = requests.get(insights_follower)
-print(r.text)
+    if os.path.exists("data/archive.json"):
+        with open("data/archive.json", "a") as json_file:
+            archive = json.dumps(instagram_follower.text, indent=4)
+            json_file.write(archive)
+    else:
+        with open("data/archive.json", "w", encoding="utf-8") as json_file:
+            archive = json.dumps(instagram_follower.text, indent=4)
+            json_file.write(archive)
